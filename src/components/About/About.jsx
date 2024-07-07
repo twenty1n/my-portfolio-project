d577a261a1563b148576f54c8d1913ba2ab5b555
twenty1n <ratirin.21n@mail.kmutt.ac.th>
2024-07-07 13:30:25 +0700

    add code and img

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,38 +5,42 @@ import { getImageUrl } from "../../utils";
 
 export const About = () => {
   return (
-    <section className={styles.container}>
+    <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
-        <img src={getImageUrl("about/aboutImage.png")} alt="Laptop" />
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Laptop"
+          className={styles.aboutImage}
+        />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
             <img
               src={getImageUrl("about/cursorIcon.png")}
               alt="Cursor Icon"
-              className={styles.aboutImage}
+              className={styles.aboutIcon}
             />
             <div className={styles.aboutItemText}>
               <h3>Frontend Developer</h3>
-              <p>I'm a frontend developer</p>
+              <p>I'm a frontend developer.</p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img
               src={getImageUrl("about/serverIcon.png")}
               alt="Server Icon"
-              className={styles.aboutImage}
+              className={styles.aboutIcon}
             />
             <div className={styles.aboutItemText}>
               <h3>Backend Developer</h3>
-              <p>I'm a backend developer</p>
+              <p>I'm a backend developer.</p>
             </div>
           </li>
           <li className={styles.aboutItem}>
             <img
               src={getImageUrl("about/cursorIcon.png")}
               alt="Cursor Icon"
-              className={styles.aboutImage}
+              className={styles.aboutIcon}
             />
             <div className={styles.aboutItemText}>
               <h3>UI Designer</h3>
